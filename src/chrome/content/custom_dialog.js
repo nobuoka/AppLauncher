fbaf6807e5b389b10ca3d164436de624a0a5d9bf
nobuoka <nobuoka@vividcode.info>
2013-02-09 17:16:59 +0900

    Move inner script in custom_dialog.xul to new JS file

---
+++ src/chrome/content/custom_dialog.js
@@ -0,0 +1,26 @@
+try {
+  // 初期化関数をロード時に実行する
+  (function() {
+      //var manager = new info.vividcode.ext.applauncher.PrefsManager();
+      var init = function() {
+          try {
+              document.documentElement.setAttribute("title", window.arguments[0].title);
+              document.documentElement.appendChild(
+                  document.createElementNS("http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul", "label") ).
+                      setAttribute("value", window.arguments[0].message);
+              window.moveToAlertPosition();
+          } catch(e) {
+              window.alert(e);
+          }
+      };
+      window.addEventListener("dialogaccept", function() {
+          window.arguments[0].returnValue = true;
+      }, false);
+      window.addEventListener("dialogcancel", function() {
+          window.arguments[0].returnValue = false;
+      }, false);
+      window.addEventListener("load", function() { init(); }, false);
+  }).call(this);
+} catch(e) {
+  window.alert(e);
+}
